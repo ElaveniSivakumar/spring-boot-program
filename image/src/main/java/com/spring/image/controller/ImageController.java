5c969bf45ce14de2b06f0326e92960effec2b63e
ElaveniSivakumar <elavenisivakumar19@gmail.com>
2023-11-18 03:23:28 -0500

    image

--- image/src/main/java/com/spring/image/controller/ImageController.java
+++ image/src/main/java/com/spring/image/controller/ImageController.java
@@ -1,18 +1,26 @@
 package com.spring.image.controller;
 
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
 import java.time.LocalDate;
 import java.util.UUID;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +29,16 @@ import org.springframework.web.multipart.MultipartFile;
 import com.spring.image.entity.ImageEntity;
 import com.spring.image.service.ImageService;
 
+
 @RestController
 @RequestMapping(value = "/image")
-public class ImageController {
+public class ImageController{
+	
+
 	
 	@Autowired
 	public ImageService imageService;
+	
 	
 	 @PostMapping("/upload")
 	 public ResponseEntity<String> uploadImage(@RequestParam("name") String name, @RequestParam("file") MultipartFile file,@RequestParam("status") String status,@RequestParam("date") String date) {
@@ -44,16 +56,48 @@ public class ImageController {
 	        }
 	    }
 	 
-	 @GetMapping("/{id}")
-	    public ResponseEntity<byte[]> downloadImage(@PathVariable UUID id) {
-	    	ImageEntity imageObj = imageService.getImageById(id);
-	        if (imageObj != null) {
-	            return ResponseEntity.ok()
-	                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + imageObj.getName())
+	@GetMapping("/{id}")
+	    public ResponseEntity<?> downloadImage(@PathVariable UUID id) throws Exception {
+	    	 
+			
+	    	try {
+
+				ImageEntity imageObj = imageService.getImageById(id);
+				imageObj.getId();
+				byte[] data = imageObj.getImage();
+				InputStream inputStream = new ByteArrayInputStream(data);
+				String fileNameText = imageObj.getName();
+				String fileName = URLEncoder.encode(fileNameText, "UTF-8").replaceAll("\\+", "%20");
+				String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+				if ("png".equals(fileExtension)){
+	            return ResponseEntity.ok().header("Access-Control-Expose-Headers", "Content-Disposition")
+	                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + imageObj.getName() +".png\"" )
+	                  
+	                    .contentType(MediaType.IMAGE_PNG)
+	                    
+	                    .body(new InputStreamResource(inputStream));
+	            
+	        }else if ("jpeg".equals(fileExtension)){
+	            return ResponseEntity.ok().header("Access-Control-Expose-Headers", "Content-Disposition")
+	                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + imageObj.getName() +".jpeg\"" )
+	                  
 	                    .contentType(MediaType.IMAGE_JPEG)
-	                    .body(imageObj.getImage());
+	                    
+	                    .body(new InputStreamResource(inputStream));
+	        }else if ("pdf".equals(fileExtension)){
+	            return ResponseEntity.ok().header("Access-Control-Expose-Headers", "Content-Disposition")
+	                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + imageObj.getName() +".pdf\"" )
+	                  
+	                    .contentType(MediaType.APPLICATION_PDF)
+	                    
+	                    .body(new InputStreamResource(inputStream));
 	        } else {
-	            return ResponseEntity.notFound().build();
+	        	return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body("Unsupported file type");
 	        }
+} catch (Exception e) {
+	e.printStackTrace();
+	return (ResponseEntity<?>) ResponseEntity.badRequest();
 }
 }
+}
+
--- image/src/main/java/com/spring/image/service/ImageService.java
+++ image/src/main/java/com/spring/image/service/ImageService.java
@@ -4,6 +4,7 @@ package com.spring.image.service;
 
 import java.util.UUID;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
